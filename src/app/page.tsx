cef9b00ac3eed6f6057fe348d32a7e8d23954a13
Chapman Cheng <chapboy1989@gmail.com>
2025-08-27 11:17:25 +0100

    basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+} from "@/components/ui/pagination";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { AccidentStat } from "@/types/accident-stat";
+import Link from "next/link";
+// json-server doesn't add a pagination decorator for you, so here you go
+const TOTAL_LENGTH = 50626;
+const LIMIT = 20;
+
+const pageNumbers = Array.from(
+  { length: TOTAL_LENGTH / LIMIT },
+  (_, i) => i + 1
+);
 export default function Home() {
   return (
     <main>
