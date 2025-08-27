62ab19a0440d2ab3656e74f0c9d381e517b12ecb
Chapman Cheng <chapboy1989@gmail.com>
2025-08-27 11:41:34 +0100

    add basic page placeholders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ import { AccidentStat } from "@/types/accident-stat";
 import Link from "next/link";
 // json-server doesn't add a pagination decorator for you, so here you go
 const TOTAL_LENGTH = 50626;
-const LIMIT = 20;
+const LIMIT = 15;
 
 const pageNumbers = Array.from(
   { length: TOTAL_LENGTH / LIMIT },
@@ -25,7 +25,35 @@ const pageNumbers = Array.from(
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Location (Borough)</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Serverity</TableHead>
+            <TableHead>Casualties Count</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {accidents.map((accident) => (
+            <TableRow key={accident.id}>
+              <TableCell>{accident.id}</TableCell>
+              <TableCell>
+                {accident.location} ({accident.borough})
+              </TableCell>
+              <TableCell>{accident.date}</TableCell>
+              <TableCell>{accident.severity}</TableCell>
+              <TableCell>{accident.casualties.length}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <p>
+        Page 1 of 3375
+      </p>
+      {/* <Pagination></Pagination */}
     </main>
   );
 }
